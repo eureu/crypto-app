55b22024e2cf16dff4c151e5a9eb4827db69658a
eureu <venekbespalov@mail.ru>
2024-09-06 19:43:55 +0300

    upd: header selector

--- react-crypto-initial/src/components/layout/CoinInfoModal.jsx
+++ react-crypto-initial/src/components/layout/CoinInfoModal.jsx
@@ -1,16 +1,26 @@
-import { Flex, Tag, Typography } from "antd";
+import { Flex, Tag, Typography, Divider } from "antd";
 
 export default function CoinInfoModal({ coin }) {
   return (
-    <Flex>
-      <img
-        src={coin.icon}
-        alt={coin.name}
-        style={{ width: 40, marginRight: 10 }}
-      ></img>
-      <Typography.Title level={2} style={{ margin: 0 }}>
-        ({coin.symbol}) {coin.name}
-      </Typography.Title>
-    </Flex>
+    <>
+      <Flex>
+        <img
+          src={coin.icon}
+          alt={coin.name}
+          style={{ width: 40, marginRight: 10 }}
+        ></img>
+        <Typography.Title level={2} style={{ margin: 0 }}>
+          ({coin.symbol}) {coin.name}
+        </Typography.Title>
+      </Flex>
+      <Divider />
+      <Typography.Paragraph>
+        <Typography.Text strong>
+          price change in 1 hour: {coin.priceChange1h}
+          price change in 1 week: {coin.priceChange1w}
+          price change in 1 month: {coin.priceChange1m}
+        </Typography.Text>
+      </Typography.Paragraph>
+    </>
   );
 }
