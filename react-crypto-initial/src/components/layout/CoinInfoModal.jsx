6a9068b752f6c633fb5aa9f2e97e8ff52c377271
eureu <venekbespalov@mail.ru>
2024-09-07 05:09:39 +0300

    upd: modal bug fix

--- react-crypto-initial/src/components/layout/CoinInfoModal.jsx
+++ react-crypto-initial/src/components/layout/CoinInfoModal.jsx
@@ -15,12 +15,37 @@ export default function CoinInfoModal({ coin }) {
       </Flex>
       <Divider />
       <Typography.Paragraph>
-        <Typography.Text strong>
-          price change in 1 hour: {coin.priceChange1h}
-          price change in 1 week: {coin.priceChange1w}
-          price change in 1 month: {coin.priceChange1m}
-        </Typography.Text>
+        <Typography.Text strong>1 hour: </Typography.Text>
+        <Tag color={coin.priceChange1h > 0 ? "green" : "red"}>
+          {coin.priceChange1h}%
+        </Tag>
+        <Typography.Text strong>1 day: </Typography.Text>
+        <Tag color={coin.priceChange1w > 0 ? "green" : "red"}>
+          {coin.priceChange1d}%
+        </Tag>
+        <Typography.Text strong>1 week: </Typography.Text>
+        <Tag color={coin.priceChange1m > 0 ? "green" : "red"}>
+          {coin.priceChange1w}%
+        </Tag>
       </Typography.Paragraph>
+      <Typography.Paragraph>
+        <Typography.Text strong>price: </Typography.Text>
+        {coin.price}
+      </Typography.Paragraph>
+      <Typography.Paragraph>
+        <Typography.Text strong>price Btc: </Typography.Text>
+        {coin.priceBtc}
+      </Typography.Paragraph>
+      <Typography.Paragraph>
+        <Typography.Text strong>Coin Market Cap: </Typography.Text>
+        {coin.marketCap}
+      </Typography.Paragraph>
+      {coin.contractAddress && (
+        <Typography.Paragraph>
+          <Typography.Text strong>Contract Address: </Typography.Text>
+          {coin.contractAddress}
+        </Typography.Paragraph>
+      )}
     </>
   );
 }
